3f33664f0bddf4923947974518415bab8b6feed3
reoanrudo <sodapon03@gmail.com>
2025-12-06 18:30:09 +0900

    プロジェクトの準備完了

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (config('app.env') == 'local') {
+            $this->call([MemoSeeder::class]);
+        }
     }
 }
