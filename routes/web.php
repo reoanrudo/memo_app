3bf4ceb7dedf6a253b41c0e3fd0d05994c8cf72b
reoanrudo <sodapon03@gmail.com>
2025-12-07 14:12:16 +0900

    メモアプリ(4) - データの更新、削除の完了

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Volt::route('/memos/create', 'memos.create')->name('memos.create');
 // 動的パラメータを含むルートは最後に定義
 // 詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
+// 編集ページ
+Volt::route('/memos/{memo}/edit', 'memos.edit')->name('memos.edit');
