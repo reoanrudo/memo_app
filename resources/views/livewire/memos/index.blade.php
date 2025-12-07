835170b8331e19d98fb6139d1f7dd1f939896cde
reoanrudo <sodapon03@gmail.com>
2025-12-07 11:54:07 +0900

    モアプリ(3) - データの登録の完了

--- resources/views/livewire/memos/index.blade.php
+++ resources/views/livewire/memos/index.blade.php
@@ -5,6 +5,10 @@ use App\Models\Memo;
 
 state(['memos' => fn() => Memo::all()]);
 
+$create = function () {
+    return redirect()->route('memos.create');
+};
+
 ?>
 
 <div>
@@ -18,5 +22,7 @@ state(['memos' => fn() => Memo::all()]);
             </li>
         @endforeach
     </ul>
+
+    <button wire:click="create">登録する</button>
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,9 @@ Route::get('/', function () {
 
 // 一覧ページ
 Volt::route('/memos', 'memos.index')->name('memos.index');
+// 具体的なパスを先に定義
+// 新規作成ページ
+Volt::route('/memos/create', 'memos.create')->name('memos.create');
+// 動的パラメータを含むルートは最後に定義
 // 詳細ページ
 Volt::route('/memos/{memo}', 'memos.show')->name('memos.show');
